e6ac64d3919491b3f8683a185f65856af516a4f9
Žan <zan@promigra.com>
2022-12-03 14:23:48 +0100

    Add project files.

---
+++ Models/Activity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sportpad.Models
+{
+    public class Activity
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public int MaximumNumber { get; set; }
+
+        public Guid LocationId { get; set; }
+        public Guid SportId { get; set; }
+
+    }
+}
---
+++ Models/Rating.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sportpad.Models
+{
+    public class Rating
+    {
+        public Guid Id { get; set; }
+        public int Rate { get; set; }
+        public string Name { get; set; }
+        public Guid UserId { get; set; }
+        public Guid PosterId { get; set; }
+    }
+}
