9eb89a14ff97373366fbd74d7723269a5565b763
Žan <zan@promigra.com>
2023-01-07 22:14:01 +0100

    dodani api kontrolerji, zaenkrat delajo GET zahteve

---
+++ Controllers/EventApiController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Sportpad.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Sportpad.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EventApiController : ControllerBase
+    {
+        // GET: api/<EventApiController>
+        [HttpGet]
+        public Event[] Get()
+        {
+            return new Event[2];
+        }
+
+        // GET api/<EventApiController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<EventApiController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<EventApiController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<EventApiController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Controllers/EventUserApiController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Sportpad.Data;
+using Sportpad.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Sportpad.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EventUserApiController : ControllerBase
+    {
+        private SportpadContext _context;
+        public EventUserApiController(SportpadContext context) {
+            _context = context; 
+        }
+        // GET: api/<EventUserApiController>
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<EventUser>>> GetEventUsers()
+        {
+            return await _context.EventUsers.ToListAsync();
+        }
+
+        // GET api/<EventUserApiController>/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<EventUser>> GetEventUser(int id)
+        {
+            var eventUser = await _context.EventUsers.FindAsync(id);  
+
+            if (eventUser == null) {
+                return NotFound();
+            }
+            return eventUser;
+        }
+
+        // POST api/<EventUserApiController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<EventUserApiController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<EventUserApiController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
